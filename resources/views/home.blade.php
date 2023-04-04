4bf04667c169a7757f22d9f13165a094e06feac2
eSSeGH <ste.virgadaula@gmail.com>
2023-04-04 16:25:00 +0200

    SETUP repo + BONUS completed

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+
+    <main>
+        <div class="container" style="max-width:900px; margin: 0 auto;">
+            <h1 style="text-align:center;">Hello World!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores optio voluptas omnis magni, sit quod quia ullam fugiat, repudiandae qui, sequi voluptates rerum dolor. Consequuntur eius ex, qui libero aliquam vitae ea molestias fugit animi, architecto iusto tenetur pariatur, numquam illum non sapiente aperiam praesentium aut dolores quibusdam facere. Tenetur?</p>
+        </div>
+        
+    </main>
+    
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('Home');
+});
+
+Route::get('/services', function () {
+    return view('Services');
+});
+
+Route::get('/about', function () {
+    return view('About');
+});
+
+Route::get('/contacts', function () {
+    return view('Contacts');
+});
+
